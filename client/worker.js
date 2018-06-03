da09655119221ae622a41b65bde3b6fcd60e52c6
ericVargas <batman@erics-mbp-2.home>
2018-06-03 03:03:00 -0400

    added local img

--- client/worker.js
+++ client/worker.js
@@ -5,6 +5,6 @@ self.addEventListener('push', e => {
     console.log('Push Recieved...');
     self.registration.showNotification(data.title, {
         body: 'Notified by Eric Vargas!',
-        icon: 'https://raw.githubusercontent.com/ericVargas/Portfolio/master/img/favicon/squareShort.png?token=APY6_pJwHRFTQFSjLp_IgoQ7RVa4lfylks5bHMm6wA%3D%3D'
+        icon: './squareShort.png'
     });
 });
