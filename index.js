917d868e6c4449f1cee8f61e7857a0d8d25388bf
ericVargas <batman@erics-mbp-2.home>
2018-06-03 01:51:13 -0400

    created route fro subscription

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const path = require('path');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 const pubicVapidKey = 'BFcaAtEZv4JqzMp49mcz4D7cmH0q-wRbNXGfPjMFgBDTZMbyvxcWWf3NHP2loI91sY2KtYyc0YDOapfHIJDyV-E';
 const privateVapidKey = 'JAiDq1N5roReOABJDoM_ubNvQyBNE0UZSd4cjVbULB0';
 
 webpush.setVapidDetails('malto:test@test.com', pubicVapidKey, privateVapidKey);
+
+// Subscribe Route
+app.post('/subscribe', (req, res) => {
+    // Get pushSubscription objct
+    const subscription = req.body;
+    
+    // Send 201 - resource created
+    res.status(201).json({});
+    
+    // Create payload
+    const payload = JSON.stringify({ title: 'Push Test' });
+    
+    // Pass object into sendNotification
+    webpush.sendNotification(subscription, payload).catch(err => console.error(err));
+});
